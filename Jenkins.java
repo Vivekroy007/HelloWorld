59445a96ded2fad613293d07b27bab8ae133fe8f
Vivek <intern.vivek.r@toranainc.com>
2023-05-09 11:58:21 +0530

    this is commited

---
+++ Jenkins.java
@@ -0,0 +1,11 @@
+public class Jenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello and Welcome");
+        for (int i = 0; i <10; i++) {
+            System.out.println("this is ur no"+i);
+            System.out.println("Hello World");
+
+        }
+    }
+
+}
